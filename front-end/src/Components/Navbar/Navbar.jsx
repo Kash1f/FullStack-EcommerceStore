127074a9b1dbe15688360fbbaeeff2bbca136c05
Kashif <ahmed.kashif1001@gmail.com>
2024-01-07 19:17:14 +0500

    navbar complete

--- front-end/src/Components/Navbar/Navbar.jsx
+++ front-end/src/Components/Navbar/Navbar.jsx
@@ -5,20 +5,23 @@ import cart_icon from "../Assets/cart_icon.png";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <div className="nav-logo">
-        <img src={logo} alt="" />
-        <p>SHOPPER</p>
-        </div>
-        <ui className="nav-menu">
-            <li>Shop</li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
-        </ui>
-
-      
+   <div className="navbar">
+    <div className="nav-logo">
+    <img src={logo} alt=""/>
+    <p>SHOPPER</p>
     </div>
+    <ul className="nav-menu">
+      <li>Shop</li>
+      <li>Men</li>
+      <li>Women</li>
+      <li>Kids</li>
+    </ul>
+    <div className="nav-login-cart">
+      <button>Login</button>
+      <img src={cart_icon} alt="" />
+    </div>
+
+   </div>
   );
 };
 
